16b3affc48c207d07aa7c45d90e2bbdfc962bf89
Sandesh Thapa <thapasandesh1@gmail.com>
2020-04-02 13:02:38 -0400

    Testing documentation

--- rungeKutta.cpp
+++ rungeKutta.cpp
@@ -1,4 +1,23 @@
-// C program to implement Runge Kutta method 
+/*!
+ * C program to implement Runge Kutta method
+ *
+ * Copyright 2020 Sandesh Thapa <sandesh.thapa@okstate.edu>
+ *
+ * This code is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the code.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include<stdio.h> 
   
 // A sample differential equation "dy/dx = (x - y)/2" 
